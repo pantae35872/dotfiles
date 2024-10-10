5f73b57361f3a93d816c7d709be7268586bd07a0
Pantae <pantae35872@gmail.com>
2024-10-11 02:19:00 +0700

    No more symlink

---
+++ sync.sh
@@ -0,0 +1,8 @@
+rm -rf hypr
+rm -rf HyprV
+rm -rf alacritty
+rm -rf nvim
+cp -r /home/pantae/.config/hypr/ hypr
+cp -r /home/pantae/.config/HyprV/ HyprV
+cp -r /home/pantae/.config/alacritty/ alacritty
+cp -r /home/pantae/.config/nvim/ nvim
